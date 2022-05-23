1ac100f3647248e077d26b2eb6e754bc86e98d12
caetanolmsouza <caetanomartins@hotmail.com>
2022-05-23 10:41:19 +0200

    moved files to top level and the code shorter

--- game.js
+++ game.js
@@ -21,35 +21,23 @@ let coronaPosition = currentPosition;
 
 const xi = new Target(34, "xi-Jinping");
 
-let XiInitialPosition = 34;
-let xiCurrentPosition = XiInitialPosition;
+const trump = new Target(36, "trump");
 
-let trumpInitialPosition = 36;
-let trumpCurrentPosition = trumpInitialPosition;
+const merkel = new Target(32, "merkel");
 
-let merkelInitialPosition = 32;
-let merkelCurrentPosition = merkelInitialPosition;
+const harry = new Target(20, "harry");
 
-let harryInitialPosition = 20;
-let harryCurrentPosition = harryInitialPosition;
+const pope = new Target(18, "pope");
 
-let popeInitialPosition = 18;
-let popeCurrentPosition = popeInitialPosition;
+const musk = new Target(22, "musk");
 
-let muskInitialPosition = 22;
-let muskCurrentPosition = muskInitialPosition;
+const macron = new Target(38, "macron");
 
-let macronInitialPosition = 38;
-let macronCurrentPosition = macronInitialPosition;
+const boris = new Target(24, "boris");
 
-let borisInitialPosition = 24;
-let borisCurrentPosition = borisInitialPosition;
+const erdogan = new Target(40, "erdogan");
 
-let erdoganInitialPosition = 40;
-let erdoganCurrentPosition = erdoganInitialPosition;
-
-let kimInitialPosition = 26;
-let kimCurrentPosition = kimInitialPosition;
+const kim = new Target(26, "kim");
 
 for (let i = 0; i < gridWidth * gridHeight; i++) {
   const cell = createCell();
@@ -139,257 +127,17 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-function showTrump(classToAdd) {
-  // Show the player in the currentPosition
-  cells[trumpCurrentPosition].classList.add("trump");
-  if (classToAdd) {
-    cells[trumpCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removeTrump() {
-  // Stop showing the player in the currentPosition
-  cells[trumpCurrentPosition].classList.remove("trump");
-}
-
-function moveTrump(trumpNewPosition, classToAdd) {
-  if (trumpNewPosition < 0) {
-    return;
-  }
-  if (trumpNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removeTrump();
-  trumpCurrentPosition = trumpNewPosition;
-
-  // Always show last
-  showTrump(classToAdd);
-}
-
-function showMerkel(classToAdd) {
-  // Show the player in the currentPosition
-  cells[merkelCurrentPosition].classList.add("merkel");
-  if (classToAdd) {
-    cells[merkelCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removeMerkel() {
-  // Stop showing the player in the currentPosition
-  cells[merkelCurrentPosition].classList.remove("merkel");
-}
-
-function moveMerkel(merkelNewPosition, classToAdd) {
-  if (merkelNewPosition < 0) {
-    return;
-  }
-  if (merkelNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removeMerkel();
-  merkelCurrentPosition = merkelNewPosition;
-
-  // Always show last
-  showMerkel(classToAdd);
-}
-
-function showHarry(classToAdd) {
-  // Show the player in the currentPosition
-  cells[harryCurrentPosition].classList.add("harry");
-  if (classToAdd) {
-    cells[harryCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removeHarry() {
-  // Stop showing the player in the currentPosition
-  cells[harryCurrentPosition].classList.remove("harry");
-}
-
-function moveHarry(harryNewPosition, classToAdd) {
-  if (harryNewPosition < 0) {
-    return;
-  }
-  if (harryNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removeHarry();
-  harryCurrentPosition = harryNewPosition;
-
-  // Always show last
-  showHarry(classToAdd);
-}
-
-function showPope(classToAdd) {
-  // Show the player in the currentPosition
-  cells[popeCurrentPosition].classList.add("pope");
-  if (classToAdd) {
-    cells[popeCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removePope() {
-  // Stop showing the player in the currentPosition
-  cells[popeCurrentPosition].classList.remove("pope");
-}
-
-function movePope(popeNewPosition, classToAdd) {
-  if (popeNewPosition < 0) {
-    return;
-  }
-  if (popeNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removePope();
-  popeCurrentPosition = popeNewPosition;
-
-  // Always show last
-  showPope(classToAdd);
-}
-
-function showMusk(classToAdd) {
-  // Show the player in the currentPosition
-  cells[muskCurrentPosition].classList.add("musk");
-  if (classToAdd) {
-    cells[muskCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removeMusk() {
-  // Stop showing the player in the currentPosition
-  cells[muskCurrentPosition].classList.remove("musk");
-}
-
-function moveMusk(muskNewPosition, classToAdd) {
-  if (muskNewPosition < 0) {
-    return;
-  }
-  if (muskNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removeMusk();
-  muskCurrentPosition = muskNewPosition;
-
-  // Always show last
-  showMusk(classToAdd);
-}
-
-function showMacron(classToAdd) {
-  // Show the player in the currentPosition
-  cells[macronCurrentPosition].classList.add("macron");
-  if (classToAdd) {
-    cells[macronCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removeMacron() {
-  // Stop showing the player in the currentPosition
-  cells[macronCurrentPosition].classList.remove("macron");
-}
-
-function moveMacron(macronNewPosition, classToAdd) {
-  if (macronNewPosition < 0) {
-    return;
-  }
-  if (macronNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removeMacron();
-  macronCurrentPosition = macronNewPosition;
-
-  // Always show last
-  showMacron(classToAdd);
-}
-
-function showBoris(classToAdd) {
-  // Show the player in the currentPosition
-  cells[borisCurrentPosition].classList.add("boris");
-  if (classToAdd) {
-    cells[borisCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removeBoris() {
-  // Stop showing the player in the currentPosition
-  cells[borisCurrentPosition].classList.remove("boris");
-}
-
-function moveBoris(borisNewPosition, classToAdd) {
-  if (borisNewPosition < 0) {
-    return;
-  }
-  if (borisNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removeBoris();
-  borisCurrentPosition = borisNewPosition;
-
-  // Always show last
-  showBoris(classToAdd);
-}
-
-function showErdogan(classToAdd) {
-  // Show the player in the currentPosition
-  cells[erdoganCurrentPosition].classList.add("erdogan");
-  if (classToAdd) {
-    cells[erdoganCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removeErdogan() {
-  // Stop showing the player in the currentPosition
-  cells[erdoganCurrentPosition].classList.remove("erdogan");
-}
-
-function moveErdogan(erdoganNewPosition, classToAdd) {
-  if (erdoganNewPosition < 0) {
-    return;
-  }
-  if (erdoganNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removeErdogan();
-  erdoganCurrentPosition = erdoganNewPosition;
-
-  // Always show last
-  showErdogan(classToAdd);
-}
-
-function showKim(classToAdd) {
-  // Show the player in the currentPosition
-  cells[kimCurrentPosition].classList.add("kim");
-  if (classToAdd) {
-    cells[kimCurrentPosition].classList.add(classToAdd);
-  }
-}
-
-function removeKim() {
-  // Stop showing the player in the currentPosition
-  cells[kimCurrentPosition].classList.remove("kim");
-}
-
-function moveKim(kimNewPosition, classToAdd) {
-  if (kimNewPosition < 0) {
-    return;
-  }
-  if (kimNewPosition > gridWidth * gridHeight - 1) {
-    return;
-  }
-  removeKim();
-  kimCurrentPosition = kimNewPosition;
-
-  // Always show last
-  showKim(classToAdd);
-}
-
 movePlayer(currentPosition);
-xi.move(1);
-moveTrump(trumpCurrentPosition);
-moveMerkel(merkelCurrentPosition);
-moveHarry(harryCurrentPosition);
-movePope(popeCurrentPosition);
-moveMusk(muskCurrentPosition);
-moveMacron(macronCurrentPosition);
-moveBoris(borisCurrentPosition);
-moveErdogan(erdoganCurrentPosition);
-moveKim(kimCurrentPosition);
+
+setInterval(() => {
+  xi.move(1);
+}, 500);
+trump.move(0);
+merkel.move(0);
+harry.move(0);
+pope.move(0);
+musk.move(0);
+macron.move(0);
+boris.move(0);
+erdogan.move(0);
+kim.move(0);
